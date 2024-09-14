636d62ab6354fbac1fe42fa0e05d2ca00ee627fd
priyans619 <priyansh.2b@gmail.com>
2024-09-14 23:39:18 +0530

    use signOut from fb to implement logout

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,6 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { auth} from '../firebase';
 
 const AuthContext = createContext();
 
@@ -6,4 +8,36 @@ export const useAuth = () => {
   return useContext(AuthContext);
 };
 
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      setCurrentUser(user);
+      setLoading(false); // Set loading to false once the user state is determined
+
+      if (user) {
+        await fetchCounts(user.uid);
+      }
+    });
+    return unsubscribe;
+  }, []);
+
+  
+
+  const logout = () => {
+    return signOut(auth);
+  };
+
+  const value = {
+    currentUser,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
