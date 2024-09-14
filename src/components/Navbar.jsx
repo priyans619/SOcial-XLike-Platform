9f41c0fde4247fbe3271b3dd3f1769f4d344d384
priyans619 <priyansh.2b@gmail.com>
2024-09-14 23:29:56 +0530

    navlink for nav menus

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { useAuth } from '../context/AuthContext';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-  const { } = useAuth();
+  const { currentUser } = useAuth();
 
   return (
     <nav className="bg-gray-50 border-b-0 shadow-md p-4 flex justify-between items-center">
       <div className="text-[#f07499] text-2xl font-bold ml-10">
         TweetX
+      </div>
+      <div className="space-x-4 mr-12">
+        <NavLink 
+          to="/feed" 
+          className={({ isActive }) => isActive ? "text-[#f07499]" : "text-black"}
+        >
+          Feed
+        </NavLink>
+        <NavLink 
+          to="/users" 
+          className={({ isActive }) => isActive ? "text-[#f07499]" : "text-black"}
+        >
+          Users
+        </NavLink>
+        <NavLink 
+          to="/profile" 
+          className={({ isActive }) => isActive ? "text-[#f07499]" : "text-black"}
+        >
+          Profile
+        </NavLink>
       </div>
     </nav>
   );
